f2321722efcba3a13516c6765a656ea73b91ddc7
chabai <7370260@qq.com>
2023-07-10 03:49:25 +0800

    2023-7-10 03:49:22

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # shadowsocks-pac
 shadowsocks pac快捷操作,一键添加白名单网址
+
+![img.png](img.png)
--- ShadowsocksPac/Form1.Designer.cs
+++ ShadowsocksPac/Form1.Designer.cs
@@ -35,44 +35,44 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            //
+            // 
             // textBox_path
-            //
+            // 
             this.textBox_path.Font = new System.Drawing.Font("宋体", 9F);
             this.textBox_path.Location = new System.Drawing.Point(103, 22);
             this.textBox_path.Name = "textBox_path";
             this.textBox_path.Size = new System.Drawing.Size(309, 21);
             this.textBox_path.TabIndex = 0;
             this.textBox_path.TextChanged += new System.EventHandler(this.textBox_path_TextChanged);
-            //
+            // 
             // exe
-            //
+            // 
             this.exe.Location = new System.Drawing.Point(17, 22);
             this.exe.Name = "exe";
             this.exe.Size = new System.Drawing.Size(80, 21);
             this.exe.TabIndex = 1;
             this.exe.Text = "安装路径";
             this.exe.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            //
+            // 
             // textBox_url
-            //
+            // 
             this.textBox_url.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.textBox_url.Location = new System.Drawing.Point(103, 66);
             this.textBox_url.Name = "textBox_url";
             this.textBox_url.Size = new System.Drawing.Size(309, 21);
             this.textBox_url.TabIndex = 2;
-            //
+            // 
             // label1
-            //
+            // 
             this.label1.Location = new System.Drawing.Point(17, 65);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(80, 21);
             this.label1.TabIndex = 3;
             this.label1.Text = "网址";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            //
+            // 
             // button1
-            //
+            // 
             this.button1.Location = new System.Drawing.Point(170, 119);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(121, 29);
@@ -80,9 +80,9 @@
             this.button1.Text = "添加白名单";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            //
+            // 
             // Form1
-            //
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(463, 160);
