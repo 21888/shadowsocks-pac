8beafe52237de8196e00a5dcfd36f35c4e4c664d
chabai <7370260@qq.com>
2023-07-10 03:47:54 +0800

    over

--- ShadowsocksPac/Form1.Designer.cs
+++ ShadowsocksPac/Form1.Designer.cs
@@ -29,90 +29,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox_path = new System.Windows.Forms.TextBox();
             this.exe = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox_url = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(115, 44);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(358, 21);
-            this.textBox1.TabIndex = 0;
-            // 
+            //
+            // textBox_path
+            //
+            this.textBox_path.Font = new System.Drawing.Font("宋体", 9F);
+            this.textBox_path.Location = new System.Drawing.Point(103, 22);
+            this.textBox_path.Name = "textBox_path";
+            this.textBox_path.Size = new System.Drawing.Size(309, 21);
+            this.textBox_path.TabIndex = 0;
+            this.textBox_path.TextChanged += new System.EventHandler(this.textBox_path_TextChanged);
+            //
             // exe
-            // 
-            this.exe.Location = new System.Drawing.Point(24, 44);
+            //
+            this.exe.Location = new System.Drawing.Point(17, 22);
             this.exe.Name = "exe";
-            this.exe.Size = new System.Drawing.Size(85, 21);
+            this.exe.Size = new System.Drawing.Size(80, 21);
             this.exe.TabIndex = 1;
             this.exe.Text = "安装路径";
             this.exe.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(115, 88);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(358, 51);
-            this.textBox2.TabIndex = 2;
-            // 
+            //
+            // textBox_url
+            //
+            this.textBox_url.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox_url.Location = new System.Drawing.Point(103, 66);
+            this.textBox_url.Name = "textBox_url";
+            this.textBox_url.Size = new System.Drawing.Size(309, 21);
+            this.textBox_url.TabIndex = 2;
+            //
             // label1
-            // 
-            this.label1.Location = new System.Drawing.Point(24, 88);
+            //
+            this.label1.Location = new System.Drawing.Point(17, 65);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 51);
+            this.label1.Size = new System.Drawing.Size(80, 21);
             this.label1.TabIndex = 3;
             this.label1.Text = "网址";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
+            //
             // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(133, 162);
+            //
+            this.button1.Location = new System.Drawing.Point(170, 119);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(107, 41);
+            this.button1.Size = new System.Drawing.Size(121, 29);
             this.button1.TabIndex = 4;
-            this.button1.Text = "不进行代理";
+            this.button1.Text = "添加白名单";
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(289, 162);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(107, 41);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "进行代理";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            //
             // Form1
-            // 
+            //
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(522, 243);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(463, 160);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox_url);
             this.Controls.Add(this.exe);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox_path);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Shadowsocks Pac 快捷操作";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
             this.ResumeLayout(false);
             this.PerformLayout();
         }
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.TextBox textBox_path;
 
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+
+        public System.Windows.Forms.TextBox textBox_url;
         private System.Windows.Forms.Label label1;
 
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label exe;
 
         #endregion
--- ShadowsocksPac/Form1.cs
+++ ShadowsocksPac/Form1.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Masuit.Tools;
+using Masuit.Tools.Files;
 
 namespace ShadowsocksPac
 {
@@ -17,8 +20,104 @@ namespace ShadowsocksPac
             InitializeComponent();
         }
 
+        INIFile ini = new INIFile(System.AppDomain.CurrentDomain.BaseDirectory + "config.ini");
+
+        private void ProgramInits()
+        {
+            textBox_path.Text = ini.IniReadValue("shadowsocks", "path");
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            ProgramInits();
+        }
+
+        private void textBox_path_TextChanged(object sender, EventArgs e)
+        {
+            ini.IniWriteValue("shadowsocks", "path", textBox_path.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 获取域名
+            var domain = getDomain();
+            if (domain == "")
+            {
+                MessageBox.Show("请输入正确的网址");
+                return;
+            }
+            // 读入txt文件
+            string[] lines = System.IO.File.ReadAllLines(textBox_path.Text + "\\pac.txt");
+            // 判断是否包含域名
+            if (lines.Any(s => s.Contains(domain)))
+            {
+                // 提示是否删除
+                DialogResult dr = MessageBox.Show("是否删除？", "提示", MessageBoxButtons.YesNo);
+                if (dr == DialogResult.Yes)
+                {
+                    // 删除
+                    lines = lines.Where(s => !s.Contains(domain)).ToArray();
+                    // 保存
+                    System.IO.File.WriteAllLines(textBox_path.Text + "\\pac.txt", lines);
+                    MessageBox.Show("删除成功");
+                    return;
+                }
+            }
+            for (var i = 0; i < lines.Length; i++)
+            {
+                // 判断是否为@@||local
+                if (lines[i].Contains("@@||local"))
+                {
+                    // 插入
+                    lines[i] = lines[i] + $"\r\n  \"@@||{domain}\",";
+                    // 保存
+                    System.IO.File.WriteAllLines(textBox_path.Text + "\\pac.txt", lines);
+                    MessageBox.Show("添加成功");
+                    return;
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// 获取域名
+        /// </summary>
+        /// <returns>domain</returns>
+        string getDomain()
+        {
+            // 判断网址是否正确
+            bool isUrl = textBox_path.Text.MatchUrl();
+            if (!isUrl)
+            {
+                MessageBox.Show("网址不正确");
+                return "";
+            }
+
+            // 将网址转换为域名
+            string url = textBox_url.Text;
+            Uri uri = new Uri(url);
+            string domain = uri.Host;
+            // 判断域名是否正确
+            if (domain == "")
+            {
+                MessageBox.Show("域名不正确");
+                return domain;;
+            }
+            // 判断文件是否存在
+            if (!System.IO.File.Exists(textBox_path.Text + "\\pac.txt"))
+            {
+                MessageBox.Show("pac.txt文件不存在");
+            }
+
+            return domain;
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            textBox_url.Focus();
+
         }
     }
+
+
 }
