ffe2cafb5ac27243d9a3402c840836c6a62a0569
chabai <7370260@qq.com>
2023-07-10 02:08:29 +0800

    1

--- ShadowsocksPac/Form1.Designer.cs
+++ ShadowsocksPac/Form1.Designer.cs
@@ -29,18 +29,91 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.exe = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(115, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(358, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // exe
+            // 
+            this.exe.Location = new System.Drawing.Point(24, 44);
+            this.exe.Name = "exe";
+            this.exe.Size = new System.Drawing.Size(85, 21);
+            this.exe.TabIndex = 1;
+            this.exe.Text = "安装路径";
+            this.exe.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(115, 88);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(358, 51);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(24, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 51);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "网址";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(133, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 41);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "不进行代理";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(289, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 41);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "进行代理";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(522, 257);
+            this.ClientSize = new System.Drawing.Size(522, 243);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.exe);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Shadowsocks Pac 快捷操作";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label exe;
 
         #endregion
     }
